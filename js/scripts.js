16f048c0308b6b42af7ef658276d4fbd06101674
Lucas Girundi <lukasgirundi@gmail.com>
2023-06-06 15:41:00 -0300

    Adição carrossel - Fotos

--- js/scripts.js
+++ js/scripts.js
@@ -47,37 +47,73 @@ function maeparTelas() {
 }
 
 function abreHome() {
-  const telas = maeparTelas();
-  telas[0].style.display = 'flex';
-  telas[1].style.display = 'none';
-  telas[2].style.display = 'none';
-  telas[3].style.display = 'none';
+  // const telas = maeparTelas();
+  // telas[0].style.display = 'flex';
+  // telas[1].style.display = 'none';
+  // telas[2].style.display = 'none';
+  // telas[3].style.display = 'none';
   telaAtual.textContent = 'home';
 }
 
 function abreFotos() {
   const telas = maeparTelas();
-  telas[0].style.display = 'none';
-  telas[1].style.display = 'flex';
-  telas[2].style.display = 'none';
-  telas[3].style.display = 'none';
+  // telas[0].style.display = 'none';
+  // telas[1].style.display = 'flex';
+  // telas[2].style.display = 'none';
+  // telas[3].style.display = 'none';
   telaAtual.textContent = 'fotos';
 }
 
 function abreSobre() {
   const telas = maeparTelas();
-  telas[0].style.display = 'none';
-  telas[1].style.display = 'none';
-  telas[2].style.display = 'flex';
-  telas[3].style.display = 'none';
+  // telas[0].style.display = 'none';
+  // telas[1].style.display = 'none';
+  // telas[2].style.display = 'flex';
+  // telas[3].style.display = 'none';
   telaAtual.textContent = 'Sobre Nós';
 }
 
 function abreDepoimentos() {
   const telas = maeparTelas();
-  telas[0].style.display = 'none';
-  telas[1].style.display = 'none';
-  telas[2].style.display = 'none';
-  telas[3].style.display = 'flex';
+  // telas[0].style.display = 'none';
+  // telas[1].style.display = 'none';
+  // telas[2].style.display = 'none';
+  // telas[3].style.display = 'flex';
   telaAtual.textContent = 'depoimentos';
 }
+
+// CARROSSEL FOTOS
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
+
+controls.forEach((control) => {
+  control.addEventListener("click", (e) => {
+    isLeft = e.target.classList.contains("arrow-left");
+
+    if (isLeft) {
+      currentItem -= 1;
+    } else {
+      currentItem += 1;
+    }
+
+    if (currentItem >= maxItems) {
+      currentItem = 0;
+    }
+
+    if (currentItem < 0) {
+      currentItem = maxItems - 1;
+    }
+
+    items.forEach((item) => item.classList.remove("current-item"));
+
+    items[currentItem].scrollIntoView({
+      behavior: "smooth",
+      inline: "center"
+    });
+
+    items[currentItem].classList.add("current-item");
+  });
+});
+
